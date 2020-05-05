2b987929c8d3524e69450e3eeff3220c57401c4b
Brian <bryantndiema@gmail.com>
2020-05-05 15:18:55 +0300

    add contant day, week, montht,male and female for getting document elemene by ID

--- js/script.js
+++ js/script.js
@@ -1,3 +1,13 @@
-const daysOfTheWeek = ["Sunday", "Tusday", "Wednesday", "Thursday", "Friday", "Saturday"];
+const weekDays = ["Sunday", "Tusday", "Wednesday", "Thursday", "Friday", "Saturday"];
 const femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
-const nameNames = ["Kwasi", "Kwando", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"]
+const nameNames = ["Kwasi", "Kwando", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+
+
+function getAkanName() {
+    const day = parseInt(document.getElementById("day").value);
+    const week = parseInt(document.getElementById("week").value);
+    const month = parseInt(document.getElementById("month").value);
+    
+    const female = document.getElementById("male").value;
+    const male = document.getElementById("male").value;
+}
